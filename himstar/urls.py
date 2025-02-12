a3c3f09ee28984728dbe6f021ce045b044f6887f
sahil Dhiman <sahildhiman98765@gmail.com>
2025-02-12 18:11:26 +0530

    template page added

--- himstar/urls.py
+++ himstar/urls.py
@@ -18,11 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import main
+from .views import main_page, contact_us, payment_page, privacy_page, terms_page, withdraw_page      
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main, name='main'),
+    path('', main_page, name='main_page'),
+    path('contact/', contact_us, name='contact_us'),
+    path('payment/', payment_page, name='payment_page'),
+    path('privacy/', privacy_page, name='privacy_page'),
+    path('terms/', terms_page, name='terms_page'),
+    path('withdraw/', withdraw_page, name='withdraw_page'),
     path('api/', include('accounts.urls')),
     path('api/', include('banners.urls')),
     path('api/', include('dashboard.urls')),
--- himstar/views.py
+++ himstar/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
 
-def main(request):
+def main_page(request):
     return render(request, 'index.html')
+
+
+def contact_us(request):
+    return render(request, 'contact.html')
+
+
+def payment_page(request):
+    return render(request, 'payment.html')
+
+
+def privacy_page(request):
+    return render(request, 'privacy.html')
+
+
+def terms_page(request):
+    return render(request, 'terms.html')
+
+
+def withdraw_page(request):
+    return render(request, 'withdraw.html')
